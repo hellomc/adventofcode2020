7cb68c938dbe8b0a177f38f8f8666d753a6370c5
hellomc <mc.adea@gmail.com>
2020-12-21 16:04:23 -0500

    Found #s that sum to 2020. Product is 866436. TODO: Clean up print statements. Look for better soln then O(n^2)

--- day1/prodsum.c
+++ day1/prodsum.c
@@ -11,49 +11,56 @@ int main(){
     int value;
     int count = 1;
     int index;
-    // expenses = (int *)malloc(sizeof(int)*200);
+    expenses = (int *)malloc(sizeof(int)*200);
 
     while(!feof(inFile) && fscanf(inFile, "%d", &value)){
         // https://stackoverflow.com/questions/19093224/read-numbers-from-file-into-a-dynamically-allocated-array
         printf("%d\n", value);
         if (expenses == NULL) {
-            expenses = (malloc(sizeof(value)));
+            // expenses = (malloc(sizeof(value)));
             *expenses = value;
         } else {
             index++;
             expenses = realloc(expenses, sizeof(expenses)*count);
             index = count - 1;
             *(expenses+index) = value;
+            count++;
         }
     }
-    printf("Expenses\n%p", &expenses); // address of pointer to int array
+    printf("Expenses\n%d", expenses[0]); // address of pointer to int array
 
     // Find the numbers that sum to 2020 and calculate the product.
     int diff = 0;
+    int num1 = 0;
     int num2 = 0;
     int prodsum = 0;
+    int addsum = 0;
+    bool isSum = false;
     for (int i = 0; i < index; i++) {
-        bool isSum = false;
-        int num1 = 0; // get value from int ptr array expenses
+        isSum = false;
+        num1 = expenses[i]; // get value from int ptr array expenses
         diff = 2020 - num1;
         // look for diff/num2 value in int ptr array expenses
         for (int i = 0; i < index; i++) {
-            int position = 0;
-            if (*(expenses+position) == diff) {
+            if (*(expenses+i) == diff) {
                 isSum = true;
                 num2 = diff;
                 break;
-            } else {
-                position++;
             }
         }
         // if found, then sum = true; product = num1 * diff; return product;
         if (isSum == true) {
             prodsum = num1 * num2;
-            printf("%d", prodsum);
+            addsum = num1 + num2;
+            printf("\n%d + %d = %d", num1, num2, addsum);
+            printf("\n%d * %d = %d", num1, num2, prodsum);
+            free(expenses);
             return prodsum;
         }
     }
+
+    printf("Sum 2020 not found");
+    free(expenses);
 
     return 0;
 }
