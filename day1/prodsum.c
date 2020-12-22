3293062df5a64bf93ae10d13936ee5cee399e1ef
hellomc <mc.adea@gmail.com>
2020-12-21 21:06:57 -0500

    Updated day1

--- day1/prodsum.c
+++ day1/prodsum.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(){
+int main()
+{
     FILE *inFile;
     inFile = fopen("input.txt", "r");
 
@@ -11,26 +12,31 @@ int main(){
     int value;
     int count = 1;
     int index;
-    expenses = (int *)malloc(sizeof(int)*200);
+    expenses = (int *)malloc(sizeof(int) * 200);
 
-    while(!feof(inFile) && fscanf(inFile, "%d", &value)){
+    while (!feof(inFile) && fscanf(inFile, "%d", &value))
+    {
         // https://stackoverflow.com/questions/19093224/read-numbers-from-file-into-a-dynamically-allocated-array
-        printf("%d\n", value);
-        if (expenses == NULL) {
+        // printf("%d\n", value);
+        if (expenses == NULL)
+        {
             // expenses = (malloc(sizeof(value)));
             *expenses = value;
-        } else {
+        }
+        else
+        {
             index++;
-            expenses = realloc(expenses, sizeof(expenses)*count);
+            expenses = realloc(expenses, sizeof(expenses) * count);
             index = count - 1;
-            *(expenses+index) = value;
+            *(expenses + index) = value;
             count++;
         }
     }
     printf("Expenses\n%d", expenses[0]); // first value in array
 
+    /*** Part 1 Two Sum ***/
     // Find the numbers that sum to 2020 and calculate the product.
-    int diff = 0;
+    /*int diff = 0;
     int num1 = 0;
     int num2 = 0;
     int prodsum = 0;
@@ -57,10 +63,45 @@ int main(){
             free(expenses);
             return prodsum;
         }
+    }*/
+
+    /*** Part 2 Three Sum ***/
+    int num1, num2, num3 = 0;
+    int prodsum = 0;
+    int addsum = 0;
+    int finalsum = 2020;
+    int partsum = 0;
+
+    for (int i = 0; i < index; i++)
+    {
+        // https://www.geeksforgeeks.org/find-a-triplet-that-sum-to-a-given-value/
+        for (int i = 0; i < index - 2; i++)
+        {
+            num1 = expenses[i];
+            for (int j = i + 1; j < index - 1; j++)
+            {
+                num2 = expenses[j];
+                for (int k = j + 1; k < index; k++)
+                {
+                    num3 = expenses[k];
+                    if ((num1 + num2 + num3) == finalsum)
+                    {
+                        // isSum = true;
+                        // break;
+                        addsum = num1 + num2 + num3;
+                        prodsum = num1 * num2 * num3;
+                        printf("\n%d + %d + %d = %d", num1, num2, num3, addsum);
+                        printf("\n%d * %d * %d = %d", num1, num2, num3, prodsum);    //Answer is: 276650720
+                        free(expenses);
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        printf("Sum 2020 not found");
+        free(expenses);
+
+        return 0;
     }
-
-    printf("Sum 2020 not found");
-    free(expenses);
-
-    return 0;
 }
